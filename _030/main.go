685e5cf8d509ca75690c579def3d999e11bdbf5f
Wataru NAKAI <watnk0806@gmail.com>
2015-10-06 09:46:57 +0900

    031. Slicing slices

--- _030/main.go
+++ _030/main.go
@@ -7,8 +7,10 @@ import (
 func main() {
 	p := []int{2, 3, 5, 7, 11, 13}
 	fmt.Println("p ==", p)
+	fmt.Println("p[1:4] ==", p[1:4])
 
-	for i := 0; i < len(p); i++ {
-		fmt.Printf("p[%d] == %d\n", i, p[i])
-	}
+	fmt.Println("p[:3] ==", p[:3])
+
+	fmt.Println("p[4:] ==", p[4:])
+
 }
